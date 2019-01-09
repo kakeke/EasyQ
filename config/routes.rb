4cbeaba4a57c88a3afaa252535a65d374159886c
kakeke <dedael2525@gmail.com>
2019-01-09 16:58:23 +0900

    [Update]Top画面、Userモデルの作成など

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: %i(new create destroy)
   resources :top, only: %i(index)
+
+  resources :users, only: [:show, :edit, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
