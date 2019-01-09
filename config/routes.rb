bc9382764a947b80266eec6e92e7b2e789958e2a
kakeke <dedael2525@gmail.com>
2019-01-09 15:23:13 +0900

    [Finish]Googleログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'top#index'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: %i(new create destroy)
+  resources :top, only: %i(index)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20190109055526_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :email
+      t.string :image
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
+      t.string :user_image_id
+      t.string :profile
+      t.datetime :last_login_at
+      t.datetime :deleted_at
+      t.boolean :admin
+      
+      t.timestamps
+    end
+  end
+end
